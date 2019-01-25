6a1e1b4b8701394eec9bae8c3cb86bb35bbc9a7d
Florians <pafloria@grouphc.net>
2019-01-25 09:57:00 +0100

    On branch master  Changes to be committed: 	deleted:    DIDRI1/Exchange/Dossiers personnels backup.pst 	deleted:    DIDRI1/Exchange/Mailbox backup.pst 	deleted:    DIDRI1/Exchange/mailox backup.pst 	deleted:    Felecia_bowman/investigation.ps1

--- Felecia_bowman/investigation.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-Mailbox -OrganizationalUnit "OU=NA,DC=grouphc,DC=net" -ResultSize Unlimited | Get-MailboxPermission -User fbowman -ResultSize Unlimited #hladanie pre spravneho usera
-#send on behalf for previous
-Get-Mailbox -OrganizationalUnit "OU=NA,DC=grouphc,DC=net" -ResultSize Unlimited | Get-MailboxPermission -User fbowman -ResultSize Unlimited | Get-Mailbox | select GrantSendOnBehalfTo
-#send as na tom s ktorym bol problem obsahuje "bfelicia" upravene na fbowman
-Get-Mailbox dmailbox   | Get-ADPermission | where {($_.ExtendedRights -like "*Send*")} | select user
